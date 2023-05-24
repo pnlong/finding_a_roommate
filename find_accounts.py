e42ec6a785d6ed5eba375625b147e71d970bf9de
Phillip Long <phillipnlong@gmail.com>
2023-05-24 15:09:04 -0700

    fixed find_accounts

--- find_accounts.py
+++ find_accounts.py
@@ -3,7 +3,7 @@
 # May 9, 2023
 
 # This script is meant to find other individuals who were admitted to muir by scraping the @ucsandiego.2027 Instagram account.
-
+ 
 # python ~/finding_a_roommate/find_accounts.py driver_address username password username_to_scrape output_directory
 
 # driver_address = filepath to Selenium Chrome Web Driver, download at: https://chromedriver.chromium.org/downloads
