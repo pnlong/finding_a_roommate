1726aa5e8fdb49e9a3cad2691e81c3ab8758ae7f
Phillip Long <phillipnlong@gmail.com>
2023-05-24 22:03:14 -0700

    fixed bot.py

--- bot.py
+++ bot.py
@@ -289,7 +289,7 @@ def try_to_post(account, account_name, checking_for_acceptance_letter):
         return_account.click()
         del return_account
         driver.wait(0.5, 1)
-        driver.send_message(text = "You information has been successfully posted. Please reach out if there are any issues.")
+        driver.send_message(text = "Your information has been successfully posted. Please reach out if there are any issues.")
     except:
         pass
     
@@ -342,7 +342,7 @@ while len(unread_messages) > 0:
     driver.wait(1, 2)
     
     # GET ACCOUNT NAME
-    account_name = unread_message.find_element("xpath", "./div/div/div[@class='x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh x1uhb9sk x1plvlek xryxfnj x1c4vz4f x2lah0s x1q0g3np xqjyukv x6s0dn4 x1oa3qoh x1nhvcw1']/span[@class='x1lliihq x1plvlek xryxfnj x1n2onr6 x193iq5w xeuugli x1fj9vlw x13faqbe x1vvkbs x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x1i0vuye xvs91rp xo1l8bm x5n08af x1tu3fi x3x7a5m x10wh9bi x1wdrske x8viiok x18hxmgj']/span[@class='x1lliihq x193iq5w x6ikm8r x10wlt62 xlyipyv xuxw1ft']").text
+    account_name = unread_message.find_element("xpath", "./div/div/div/span/span").text
     account = unread_message.find_element("xpath", "//a[@class='x1i10hfl x1qjc9v5 xjbqb8w xjqpnuy xa49m3k xqeqjp1 x2hbi6w x13fuv20 xu3j5b3 x1q0q8m5 x26u7qi x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xdl72j9 x2lah0s xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r x2lwn1j xeuugli xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 x16tdsg8 x1hl2dhg xggy1nq x1ja2u2z x1t137rt x1o1ewxj x3x9cwd x1e5q0jg x13rtm0m x1q0g3np x87ps6o x1lku1pv x1a2a7pz xjp7ctv xeq5yr9']").get_attribute("href")
     account = account.split("/")[-1]
     
